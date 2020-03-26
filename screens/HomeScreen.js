1947a25c3e4ce434754f0556fb8a9ad420cc4537
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2020-03-26 16:20:26 -0300

    Add carousel to  home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,9 +3,21 @@ import { View, Text, StyleSheet, Dimensions, StatusBar } from 'react-native'
 import styled from 'styled-components/native'
 import MapView from 'react-native-maps'
 import { LinearGradient } from 'expo-linear-gradient'
+import Carousel from 'react-native-snap-carousel'
 
 const screenWidth = Dimensions.get('window').width
 const screenHeight = Dimensions.get('window').height
+
+const pickupGames = [
+  {
+    name: 'Baskeire',
+    address: 'Rua Cariaçu, 120',
+  },
+  {
+    name: 'IBB - Betânia',
+    address: 'Quadra da igreja',
+  }
+]
 
 const Container = styled.View`
   width: ${screenWidth};
@@ -16,13 +28,14 @@ const Container = styled.View`
 
 const Content = styled.View`
   position: relative;
-  padding: 32px;
-  margin-bottom: 50px;
+  padding: 32px 0;
+  margin-bottom: 25px;
   background-color: #fff;
   z-index: 3;
 `
 
 const Card = styled.View`
+  height: 100px;
   padding: 32px 24px;
   flex-direction: row;
   justify-content: space-between;
@@ -70,22 +83,35 @@ const mapStyles = {
   zIndex: 1
 }
 
+const renderItem = ({ item }) => {
+  return (
+    <View style={{ justifyContent: 'center', height: 150 }}>
+      <Card>
+        <View>
+          <Title>{item.name}</Title>
+          <Subtitle>{item.address}</Subtitle>
+        </View>
+        <View>
+          <Text style={{ textAlign: 'right' }}>Today</Text>
+          <StyledTime>19:00</StyledTime>
+        </View>
+      </Card>
+    </View>
+  )
+}
+
 const HomeScreen = () => {
   return (
     <Container>
       <StatusBar barStyle="dark-content" />
       <MapView style={mapStyles} />
       <Content>
-        <Card>
-          <View>
-            <Title>Baskeire</Title>
-            <Subtitle>Rua Cariaçu, 120</Subtitle>
-          </View>
-          <View>
-            <Text style={{ textAlign: 'right' }}>Today</Text>
-            <StyledTime>19:00</StyledTime>
-          </View>
-        </Card>
+        <Carousel
+          data={pickupGames}
+          renderItem={renderItem}
+          sliderWidth={screenWidth}
+          itemWidth={screenWidth - 64}
+        />
         <LinearGradient
           colors={['rgba(255, 255, 255, 0)', 'rgba(255, 255, 255, 0.4)', '#fff']}
           style={gradientStyles}
