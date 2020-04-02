25b9170407bac492e0c4c4360b7e6729c7e20dd2
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2020-04-02 16:38:24 -0300

    Add google sign in flow

--- auth/config.js
+++ auth/config.js
@@ -1,10 +1,14 @@
+import getEnvVars from '../environment'
+
+const { firebaseApiKey, firebaseAppId } = getEnvVars()
+
 export default {
-  apiKey: 'AIzaSyBg1PnPxJC3CJtg4mC9jSR5Vigg4dGaGF8',
+  apiKey: firebaseApiKey,
   authDomain: 'ballers-mobile.firebaseapp.com',
   databaseURL: 'https://ballers-mobile.firebaseio.com',
   projectId: 'ballers-mobile',
   storageBucket: 'ballers-mobile.appspot.com',
   messagingSenderId: '147359040605',
-  appId: '1:147359040605:web:14495ade03bf12a70df9b4',
+  appId: firebaseAppId,
   measurementId: 'G-3MWNLXHG8S',
 }
