98f0f9791ee5d2ecbd2fe094a570217b9cee439f
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2020-04-06 13:41:13 -0300

    Add loading to MainStackNavigation

--- src/navigations/MainStackNavigation.js
+++ src/navigations/MainStackNavigation.js
@@ -1,39 +1,45 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import firebase from 'firebase'
 
 import MainTabNavigation from './MainTabNavigation'
 import AuthStackNavigation from './AuthStackNavigation'
+import { Loading } from '../components'
 import store from '../store'
 
 const Stack = createStackNavigator()
 
 const MainStackNavigation = () => {
   const { state, dispatch } = useContext(store)
+  const [loading, setLoading] = useState(false)
   const { currentUser } = state
 
   useEffect(() => {
+    setLoading(true)
     firebase.auth().onAuthStateChanged((user) => {
       dispatch({ type: 'get_current_user', payload: user })
+      setLoading(false)
     })
   }, [currentUser])
 
   return (
-    <Stack.Navigator>
-      {!currentUser ? (
-        <Stack.Screen
-          name="Auth"
-          component={AuthStackNavigation}
-          options={{ headerShown: false }}
-        />
-      ) : (
-        <Stack.Screen
-          name="App"
-          component={MainTabNavigation}
-          options={{ headerShown: false }}
-        />
-      )}
-    </Stack.Navigator>
+    <Loading isActive={loading}>
+      <Stack.Navigator>
+        {!currentUser ? (
+          <Stack.Screen
+            name="Auth"
+            component={AuthStackNavigation}
+            options={{ headerShown: false }}
+          />
+        ) : (
+          <Stack.Screen
+            name="App"
+            component={MainTabNavigation}
+            options={{ headerShown: false }}
+          />
+        )}
+      </Stack.Navigator>
+    </Loading>
   )
 }
 
